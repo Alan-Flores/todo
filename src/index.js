37f45958b8c989d9a62131b22bf4977c3792bb0c
Alan-Flores <alanflores@hotmail.cl>
2022-06-21 21:48:33 -0400

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { Todo } from './classes/todo.class';
-
 import './styles.css';
 
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const tarea = new Todo('Aprender JavaScript');
+export const todoList = new TodoList();
 
-console.log(tarea);
+todoList.todos.forEach( crearTodoHtml);
+
+console.log('todos', todoList.todos);
